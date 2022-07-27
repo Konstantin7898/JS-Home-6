e6f9a8861de44ff7cfd5cf8e89a37b843df22fa5
Konstantin7898 <lexus7898@gmail.com>
2022-07-28 00:24:58 +0300

    hom-js6

--- task-04.js
+++ task-04.js
@@ -1,15 +1,14 @@
 const span = document.querySelector("#value");
 const decrBtn = document.querySelector('[data-action="decrement"]');
 const incrBtn = document.querySelector('[data-action="increment"]');
+let counterValue = 0;
 
 incrBtn.addEventListener("click", () => {
-  let counterValue = parseInt(span.textContent);
   counterValue += 1;
   span.textContent = counterValue;
 });
 
 decrBtn.addEventListener("click", () => {
-  let counterValue = parseInt(span.textContent);
   counterValue -= 1;
   span.textContent = counterValue;
 });
